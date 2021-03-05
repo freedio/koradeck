3eab8bf492c8814cff674e393869b9ae830c307d
freedio <dom@coradec.com>
2021-03-05 22:53:41 +0100

    extend SqlSelection by static ALL

--- CoraDB/src/main/kotlin/com/coradec/coradeck/db/ctrl/impl/SqlSelection.kt
+++ CoraDB/src/main/kotlin/com/coradec/coradeck/db/ctrl/impl/SqlSelection.kt
@@ -3,7 +3,7 @@ package com.coradec.coradeck.db.ctrl.impl
 import com.coradec.coradeck.db.ctrl.Selection
 import com.coradec.coradeck.db.util.toSqlObjectName
 
-open class SqlSelection(val expr: String) : Selection {
+open class SqlSelection(private val expr: String) : Selection {
     private val offsetNum: Int by lazy { OFFSET.find(expr)?.groupValues?.let { if (it.size > 2) it[2].toInt() else 0 } ?: 0 }
     private val limitNum: Int by lazy { LIMIT.find(expr)?.groupValues?.let { if (it.size > 2) it[2].toInt() else 0 } ?: 0 }
     private val whereList: List<String> by lazy {
@@ -43,17 +43,18 @@ open class SqlSelection(val expr: String) : Selection {
     }
 
     companion object {
-        val OFFSET = Regex("(OFFSET|Offset|offset):(\\d+)")
-        val LIMIT = Regex("(LIMIT|Limit|limit):(\\d+)")
-        val WHERE1 = Regex("\\[(\\w+)\\s*(=|<=|>=|<>|>|<|" +
+        val ALL = SqlSelection("")
+        private val OFFSET = Regex("(OFFSET|Offset|offset):(\\d+)")
+        private val LIMIT = Regex("(LIMIT|Limit|limit):(\\d+)")
+        private val WHERE1 = Regex("\\[(\\w+)\\s*(=|<=|>=|<>|>|<|" +
                 "\\s+IN\\s+|\\s+[Ii]n\\s+|\\s+NOT\\s+IN\\s+|\\s+[Nn]ot\\s+[Ii]n\\s+|" +
                 "\\s+LIKE\\s+|\\s+[lL]ike\\s+|\\s+NOT\\s+LIKE\\s+|\\s+[Nn]ot\\s+[Ll]ike\\s+|" +
                 "\\s+BETWEEN\\s+|\\s+[Bb]etween\\s+|\\s+NOT\\s+BETWEEN\\s+|\\s+[Nn]ot\\s+[Bb]etween\\s+|" +
                 "\\s+IS\\s+DISTINCT\\s+FROM\\s+|\\s+[Ii]s\\s+[Dd]istinct\\s+[Ff]rom\\s+|" +
                 "\\s+IS\\s+NOT\\s+DISTINCT\\s+FROM\\s+|\\s+[Ii]s\\s+[Nn]ot\\s+[Dd]istinct\\s+[Ff]rom\\s+)" +
                 "\\s*([^]]+)]")
-        val WHERE2 = Regex("\\[(\\w+)\\s+(EXISTS|[Ee]xists|" +
+        private val WHERE2 = Regex("\\[(\\w+)\\s+(EXISTS|[Ee]xists|" +
                 "IS\\s+NULL|[Ii]s\\s+[Nn]ull|IS\\s+NOT\\s+NULL|[Ii]s\\s+[Nn]ot\\s+[Nn]ull)]")
-        val ORDER = Regex("(\\w+):(ASC|DESC|Asc|Desc|asc|desc)")
+        private val ORDER = Regex("(\\w+):(ASC|DESC|Asc|Desc|asc|desc)")
     }
 }
