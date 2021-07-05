b55b43d5daa3b70cf3107fb3903dc504a7c3148d
freedio <dom@coradec.com>
2021-07-05 22:12:21 +0200

    implement Set of Path type converter

---
+++ CoraTypeBasic/src/main/kotlin/com/coradec/coradeck/type/converter/Set_of_java_nio_file_Path_Converter.kt
@@ -0,0 +1,23 @@
+package com.coradec.coradeck.type.converter
+
+import com.coradec.coradeck.type.ctrl.impl.BasicTypeConverter
+import java.nio.file.Path
+import java.nio.file.Paths
+import kotlin.reflect.KTypeProjection
+import kotlin.reflect.KVariance
+import kotlin.reflect.full.createType
+import kotlin.reflect.full.starProjectedType
+
+class Set_of_java_nio_file_Path_Converter: BasicTypeConverter<Set<Path>>(
+    Set::class.createType(listOf(KTypeProjection(KVariance.OUT, Path::class.starProjectedType)))
+) {
+    override fun decodeFrom(value: String): Set<Path>? {
+        println("Decoding «%s» to a set of path".format(value))
+        TODO("Not yet implemented")
+    }
+
+    override fun convertFrom(value: Any): Set<Path>? = when(value) {
+        is List<*> -> value.map { Paths.get(it.toString()) }.toSet()
+        else -> null
+    }
+}
