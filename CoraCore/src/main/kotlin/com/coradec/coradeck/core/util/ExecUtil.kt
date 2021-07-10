f1bfebc26fc1d77021702dcf47c3a7b32b5bff1d
freedio <dom@coradec.com>
2021-07-10 10:28:50 +0200

    enhance stack frame matching in ExecUtil

--- CoraCore/src/main/kotlin/com/coradec/coradeck/core/util/ExecUtil.kt
+++ CoraCore/src/main/kotlin/com/coradec/coradeck/core/util/ExecUtil.kt
@@ -12,19 +12,29 @@ import kotlin.reflect.KClass
 
 private const val base = 3
 val caller: StackFrame get() = getStackFrame(base)
-val caller2: StackFrame get() = getStackFrame(base+1)
-val caller3: StackFrame get() = getStackFrame(base+2)
+val caller2: StackFrame get() = getStackFrame(base + 1)
+val caller3: StackFrame get() = getStackFrame(base + 2)
 val callerStack: StackTrace get() = StackTrace(Thread.currentThread().stackTrace.map { StackFrame(it) })
 val here: StackFrame = caller
 
-fun getStackFrame(index: Int): StackFrame =
-        StackFrame(Thread.currentThread().stackTrace.filter { !it.methodName.startsWith("access$") }[index])
-fun callerStackFrame(after: String): StackFrame =
-        StackFrame(Thread.currentThread().stackTrace.dropWhile { after !in it.className }.dropWhile { after in it.className }.first())
+fun getStackFrame(index: Int): StackFrame = StackFrame(Thread.currentThread().stackTrace
+    .drop(index)
+    .dropWhile { it.methodName.startsWith("access$") }
+    .dropWhile { it.className.startsWith("jdk.") || it.className.startsWith("java.") }
+    .dropWhile { "${it.className}.${it.methodName}" == "com.coradec.coradeck.core.util.ExecUtilKt.<clinit>" }
+    .first()
+)
+
+fun callerStackFrame(after: String): StackFrame = StackFrame(Thread.currentThread().stackTrace
+    .dropWhile { after !in it.className }
+    .dropWhile { after in it.className }
+    .first())
+
 fun relax() {}
 fun Thread.whenTerminated(action: (Thread) -> Unit) {
     ThreadMonitor.registerTerminationHook(this, action)
 }
+
 fun Any?.swallow() {}
 
 fun resource(klass: KClass<*>, ext: String) = ClassPathResource(klass, ext)
