705d0c8ebccdf311d6c27a348dd026115555c498
freedio <dom@coradec.com>
2021-07-25 18:07:06 +0200

    fix return type of onMessage

--- CoraControl/src/main/kotlin/com/coradec/coradeck/ctrl/ctrl/impl/BasicAgent.kt
+++ CoraControl/src/main/kotlin/com/coradec/coradeck/ctrl/ctrl/impl/BasicAgent.kt
@@ -49,7 +49,7 @@ open class BasicAgent : Logger(), Agent {
         approvedCommands.removeAll(cmd.toList())
     }
 
-    override fun onMessage(message: Information) = when (message) {
+    override fun onMessage(message: Information): Unit = when (message) {
         is Command ->
             if (approvedCommands.any { it.isInstance(message) })
                 try { message.execute() } catch (e: Throwable) {
