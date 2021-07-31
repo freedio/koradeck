ef398415d9a760edaf5eaebb80e954bf0f61955a
freedio <dom@coradec.com>
2021-07-31 16:16:59 +0200

    finish agent tests

--- IT/src/test/kotlin/com/coradec/coradeck/ctrl/impl/BasicAgentTest.kt
+++ IT/src/test/kotlin/com/coradec/coradeck/ctrl/impl/BasicAgentTest.kt
@@ -1,14 +1,16 @@
 package com.coradec.coradeck.ctrl.impl
 
 import com.coradec.coradeck.com.model.Recipient
+import com.coradec.coradeck.com.model.impl.BasicCommand
 import com.coradec.coradeck.com.model.impl.BasicRequest
 import com.coradec.coradeck.com.module.CoraComImpl
-import com.coradec.coradeck.com.trouble.RequestCancelledException
 import com.coradec.coradeck.conf.module.CoraConfImpl
 import com.coradec.coradeck.core.model.Origin
 import com.coradec.coradeck.core.util.here
 import com.coradec.coradeck.ctrl.ctrl.impl.BasicAgent
 import com.coradec.coradeck.ctrl.module.CoraControlImpl
+import com.coradec.coradeck.ctrl.trouble.CommandNotApprovedException
+import com.coradec.coradeck.ctrl.trouble.NoRouteForMessageException
 import com.coradec.coradeck.dir.model.module.CoraModules
 import com.coradec.coradeck.text.module.CoraTextImpl
 import com.coradec.coradeck.type.module.impl.CoraTypeImpl
@@ -35,6 +37,7 @@ internal class BasicAgentTest {
         // when:
         testee.inject(TestRouteRequest(here, testee))
         testee.inject(TestRouteRequest(here, testee)).standBy()
+        Thread.sleep(100)
         // then:
         assertThat(testee.received).isEqualTo(2)
     }
@@ -49,17 +52,54 @@ internal class BasicAgentTest {
             testee.inject(TestRouteRequest(here, testee)).standBy()
             // then:
             fail("Expected RequestCancelledException!")
-        } catch (e: RequestCancelledException) {
+        } catch (e: NoRouteForMessageException) {
             // expected that
         }
     }
 
     @Test fun testApproved() {
-        TODO("Not yet implemented")
+        // given:
+        var received = 0
+        class TestRouteCommand(origin: Origin, recipient: Recipient): BasicCommand(origin, recipient) {
+            override fun execute() {
+                ++received
+                succeed()
+            }
+        }
+
+        val testee = object: BasicAgent() {
+            init {
+                approve(TestRouteCommand::class)
+                synchronize()
+            }
+        }
+        // when:
+        testee.inject(TestRouteCommand(here, testee))
+        testee.inject(TestRouteCommand(here, testee)).standBy()
+        Thread.sleep(100)
+        // then:
+        assertThat(received).isEqualTo(2)
     }
 
     @Test fun testUnapproved() {
-        TODO("Not yet implemented")
+        // given:
+        var received = 0
+        class TestRouteCommand(origin: Origin, recipient: Recipient): BasicCommand(origin, recipient) {
+            override fun execute() {
+                ++received
+                succeed()
+            }
+        }
+
+        val testee = BasicAgent()
+        // when:
+        try {
+            testee.inject(TestRouteCommand(here, testee)).standBy()
+            // then:
+            fail("Expected CommandNotApprovedException!")
+        } catch (e: CommandNotApprovedException) {
+            // expected that
+        }
     }
 
     class TestRouteRequest(origin: Origin, recipient: Recipient): BasicRequest(origin, recipient)
