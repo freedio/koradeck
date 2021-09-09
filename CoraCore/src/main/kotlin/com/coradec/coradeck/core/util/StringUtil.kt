05c04da802c359aff0adedd87ec6476f1a725dac
freedio <dom@coradec.com>
2021-09-09 22:18:37 +0200

    fix recursive properties

--- CoraCore/src/main/kotlin/com/coradec/coradeck/core/util/StringUtil.kt
+++ CoraCore/src/main/kotlin/com/coradec/coradeck/core/util/StringUtil.kt
@@ -5,17 +5,20 @@
 package com.coradec.coradeck.core.util
 
 import com.coradec.coradeck.core.model.Null
+import com.coradec.coradeck.core.trouble.BasicException
 
-val Any?.formatted: String
-    get() {
-        return when (this) {
-            is Map<*, *> -> entries.joinToString(", ", "[", "]") { (key, value) -> "$key: ${value.formatted}" }
-            is String -> "\"${toString()}\""
-            null -> "«null»"
-            is Null -> "«null»"
-            else -> toString()
-        }
-    }
+val Any?.formatted: String get() = formatAnyOrNull(this, emptySet<Any>(), this)
+fun Any?.formatted(known: Set<Any?> = emptySet<Any>()) = formatAnyOrNull(this, known, this)
+private fun formatAnyOrNull(obj: Any?, known: Set<Any?>, new: Any?): String = when (obj) {
+    null -> "‹null›"
+    is Null -> "‹null›"
+    in known -> "‹known (see above)›"
+    is Map<*, *> -> obj.entries.joinToString(", ", "[", "]") { (key, value) -> "$key: ${formatAnyOrNull(value, known + new, obj)}" }
+    is String -> "\"$obj\""
+    is BasicException -> "${obj.classname}: ${obj.message(known)}"
+    is com.coradec.coradeck.core.model.Formattable -> obj.format(known)
+    else -> obj.toString()
+}
 
 operator fun Regex.contains(x: String): Boolean = x.matches(this)
 fun String.trimIfBlank() = ifBlank { "" }
