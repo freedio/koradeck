e9369744dc3315fe858a03e1b158c6b9a412bcb5
freedio <dom@coradec.com>
2021-09-26 14:30:58 +0200

    fix generic type distinction in TypeUtil when comparing types

--- CoraCore/src/main/kotlin/com/coradec/coradeck/core/util/TypeUtil.kt
+++ CoraCore/src/main/kotlin/com/coradec/coradeck/core/util/TypeUtil.kt
@@ -12,5 +12,25 @@ import kotlin.reflect.jvm.jvmErasure
 val KType.name: String
     get() = (classifier as KClass<*>).classname +
             (arguments.joinToString { it.type?.name ?: "*" }.let { if (it.isBlank()) "" else "<${it}>" })
+
 operator fun KType.contains(other: KType) = isSubtypeOf(other)
-operator fun KType.contains(instance: Any) = this.jvmErasure.isInstance(instance)
+operator fun KType.contains(instance: Any) = jvmErasure.isInstance(instance) && arguments.isEmpty() || when (jvmErasure) {
+    in Collection::class -> checkCollectionMember(this, instance as Collection<*>)
+    in Map::class -> checkMapMember(this, instance as Map<*, *>)
+    else -> false // we assume that other types of generics don't match just like that
+}
+
+private fun checkCollectionMember(type: KType, instance: Collection<*>): Boolean =
+    type.arguments.map { type.jvmErasure }.let { elementTypes ->
+        if (elementTypes.size != 1) throw IllegalStateException("Expected one type argument, but got ${elementTypes.size}!")
+        val elementType = elementTypes[0]
+        instance.isEmpty() || elementType.isInstance(instance.iterator().next())
+    }
+
+private fun checkMapMember(type: KType, instance: Map<*, *>): Boolean =
+    type.arguments.map { type.jvmErasure }.let { elementTypes ->
+        if (elementTypes.size != 2) throw IllegalStateException("Expected two type arguments, but got ${elementTypes.size}!")
+        val keyType = elementTypes[0]
+        val valueType = elementTypes[1]
+        instance.isEmpty() || with(instance.iterator().next()) { keyType.isInstance(key) && valueType.isInstance(value) }
+    }
