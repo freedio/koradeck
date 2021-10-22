39df5865ab3d7ce2b42bb1b4ef7f6a40423d99b3
freedio <dom@coradec.com>
2021-10-22 13:25:54 +0200

    improve logging

--- CoraCom/src/main/kotlin/com/coradec/coradeck/com/ctrl/Log.kt
+++ CoraCom/src/main/kotlin/com/coradec/coradeck/com/ctrl/Log.kt
@@ -11,6 +11,7 @@ import com.coradec.coradeck.com.model.StringLogEntry
 import com.coradec.coradeck.com.model.TextLogEntry
 import com.coradec.coradeck.core.model.StackFrame
 import com.coradec.coradeck.core.util.caller2
+import com.coradec.coradeck.core.util.caller3
 import com.coradec.coradeck.text.model.LocalText
 import com.coradec.coradeck.text.model.Text
 
@@ -22,25 +23,25 @@ interface Log {
     }
 
     fun log(entry: LogEntry)
-    fun abort(problem: Throwable) = log(ProblemLogEntry(caller2, FATAL, problem))
-    fun abort(template: Text, vararg args: Any) = log(TextLogEntry(caller2, FATAL, template, *args))
-    fun error(problem: Throwable) = log(ProblemLogEntry(caller2, ERROR, problem))
-    fun error(template: Text, vararg args: Any) = log(TextLogEntry(caller2, ERROR, template, *args))
-    fun error(problem: Throwable, template: Text, vararg args: Any) = log(ProblemLogEntry(caller2, ERROR, problem, template, *args))
-    fun warn(template: Text, vararg args: Any)  = log(TextLogEntry(caller2, WARNING, template, *args))
-    fun warn(problem: Throwable, template: Text, vararg args: Any) = log(ProblemLogEntry(caller2, WARNING, problem, template, *args))
-    fun alert(template: Text, vararg args: Any) = log(TextLogEntry(caller2, ALERT, template, *args))
-    fun info(template: Text, vararg args: Any) = log(TextLogEntry(caller2, INFORMATION, template, *args))
-    fun detail(template: Text, vararg args: Any) = log(TextLogEntry(caller2, DETAIL, template, *args))
-    fun detail(text: String, vararg args: Any) = log(StringLogEntry(caller2, DETAIL, text, *args))
-    fun debug(template: Text, vararg args: Any) = log(TextLogEntry(caller2, DEBUG, template, *args))
-    fun debug(text: String, vararg args: Any) = log(StringLogEntry(caller2, DEBUG, text, *args))
-    fun trace(template: Text, vararg args: Any) = log(TextLogEntry(caller2, TRACE, template, *args))
-    fun trace(text: String, vararg args: Any) = log(StringLogEntry(caller2, TRACE, text, *args))
-    fun subtrace(template: Text, vararg args: Any) = log(TextLogEntry(caller2, SUBTRACE, template, *args))
-    fun subtrace(text: String, vararg args: Any) = log(StringLogEntry(caller2, SUBTRACE, text, *args))
-    fun infra(template: Text, vararg args: Any) = log(TextLogEntry(caller2, INFRA, template, *args))
-    fun infra(text: String, vararg args: Any) = log(StringLogEntry(caller2, INFRA, text, *args))
+    fun abort(problem: Throwable) = log(ProblemLogEntry(caller3, FATAL, problem))
+    fun abort(template: Text, vararg args: Any) = log(TextLogEntry(caller3, FATAL, template, *args))
+    fun error(problem: Throwable) = log(ProblemLogEntry(caller3, ERROR, problem))
+    fun error(template: Text, vararg args: Any) = log(TextLogEntry(caller3, ERROR, template, *args))
+    fun error(problem: Throwable, template: Text, vararg args: Any) = log(ProblemLogEntry(caller3, ERROR, problem, template, *args))
+    fun warn(template: Text, vararg args: Any)  = log(TextLogEntry(caller3, WARNING, template, *args))
+    fun warn(problem: Throwable, template: Text, vararg args: Any) = log(ProblemLogEntry(caller3, WARNING, problem, template, *args))
+    fun alert(template: Text, vararg args: Any) = log(TextLogEntry(caller3, ALERT, template, *args))
+    fun info(template: Text, vararg args: Any) = log(TextLogEntry(caller3, INFORMATION, template, *args))
+    fun detail(template: Text, vararg args: Any) = log(TextLogEntry(caller3, DETAIL, template, *args))
+    fun detail(text: String, vararg args: Any) = log(StringLogEntry(caller3, DETAIL, text, *args))
+    fun debug(template: Text, vararg args: Any) = log(TextLogEntry(caller3, DEBUG, template, *args))
+    fun debug(text: String, vararg args: Any) = log(StringLogEntry(caller3, DEBUG, text, *args))
+    fun trace(template: Text, vararg args: Any) = log(TextLogEntry(caller3, TRACE, template, *args))
+    fun trace(text: String, vararg args: Any) = log(StringLogEntry(caller3, TRACE, text, *args))
+    fun subtrace(template: Text, vararg args: Any) = log(TextLogEntry(caller3, SUBTRACE, template, *args))
+    fun subtrace(text: String, vararg args: Any) = log(StringLogEntry(caller3, SUBTRACE, text, *args))
+    fun infra(template: Text, vararg args: Any) = log(TextLogEntry(caller3, INFRA, template, *args))
+    fun infra(text: String, vararg args: Any) = log(StringLogEntry(caller3, INFRA, text, *args))
 
     // -----------------------------------------------------
 
