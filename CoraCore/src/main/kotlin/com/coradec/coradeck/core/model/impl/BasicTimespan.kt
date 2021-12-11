32f01bd74172ceffe4eaec9dcbfce7d045b00b86
freedio <dom@coradec.com>
2021-12-11 14:20:59 +0100

    implement BasicTimespan.toString()

--- CoraCore/src/main/kotlin/com/coradec/coradeck/core/model/impl/BasicTimespan.kt
+++ CoraCore/src/main/kotlin/com/coradec/coradeck/core/model/impl/BasicTimespan.kt
@@ -6,7 +6,20 @@ package com.coradec.coradeck.core.model.impl
 
 import com.coradec.coradeck.core.model.Timespan
 import java.util.concurrent.TimeUnit
+import java.util.concurrent.TimeUnit.*
 
 data class BasicTimespan(override val amount: Long, override val unit: TimeUnit) : Timespan {
     override val representation = "%d %s".format(amount, unit.name)
+
+    override fun toString() = "%d %s".format(amount, unit.code)
+
+    private val TimeUnit.code: String get() = when(this) {
+        SECONDS -> "s"
+        NANOSECONDS -> "ns"
+        MICROSECONDS -> "μs"
+        MILLISECONDS -> "ms"
+        MINUTES -> "m"
+        HOURS -> "h"
+        DAYS -> "d"
+    }
 }
