98ef33a903122037be962abd542c56676889e152
freedio <dom@coradec.com>
2022-01-16 19:29:48 +0100

    moved WorkFinishedEvent and WorkStartedEvent to coracom, because may apps use it

---
+++ CoraComBasic/src/main/kotlin/com/coradec/coradeck/com/com/WorkStartedEvent.kt
@@ -0,0 +1,10 @@
+/*
+ * Copyright ⓒ 2018 − 2021 by Coradec LLC.  All rights reserved.
+ */
+
+package com.coradec.coradeck.com.com
+
+import com.coradec.coradeck.com.model.impl.BasicEvent
+import com.coradec.coradeck.core.model.Origin
+
+class WorkStartedEvent(origin: Origin): BasicEvent(origin)
